a4904076a11e7f2f136198ce925089b7ad6d58a7
Bhupendra Parihar <someone@nowhere.com>
2015-05-13 15:59:24 +0530

    URL changed to use myjson

--- www/js/menuService.js
+++ www/js/menuService.js
@@ -4,7 +4,10 @@ service
 	.factory('menuService', function($http) {
 		return {
 			getMenu: function() {
-				return $http.get("http://localhost:8000/api/movies"); //https://api.myjson.com/bins/37rl5"); // "/json/menu.json"
+				return $http.get("https://api.myjson.com/bins/4eih5"); //$http.get("http://localhost:8000/api/movies"); //https://api.myjson.com/bins/37rl5"); // "/json/menu.json"
+			},
+			getDummyValue : function(){
+				return true;
 			}
 		}
 	});
