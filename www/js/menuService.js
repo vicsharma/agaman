041f2a4189f56473a8e693a77dd1521c488efd81
hnsreddy <hnsreddy@gmail.com>
2015-06-20 11:56:41 +0530

    Bathinda Junction Menu
    Adding some actual menu items for testing.

--- www/js/menuService.js
+++ www/js/menuService.js
@@ -4,7 +4,7 @@ service
 	.factory('menuService', function($http) {
 		return {
 			getMenu: function() {
-				return $http.get("https://api.myjson.com/bins/4eih5"); //$http.get("http://localhost:8000/api/movies"); //https://api.myjson.com/bins/37rl5"); // "/json/menu.json"
+				return $http.get("https://api.myjson.com/bins/2qtt8"); //$http.get("http://localhost:8000/api/movies"); //https://api.myjson.com/bins/37rl5"); // "/json/menu.json"
 			},
 			getDummyValue : function(){
 				return true;
